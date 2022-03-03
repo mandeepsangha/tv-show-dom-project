af6057c868071257d625ac19ae57718198387923
mandeepsangha <91365504+mandeepsangha@users.noreply.github.com>
2022-03-03 16:04:56 +0000

    Level 300 completed

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ function setup() {
   });
 }
 
+const showsView = document.getElementById("showsView");
+
 function makePageForEpisodes(episodeList) {
   const episode_count = document.getElementById("episode-count");
   const rootElem = document.getElementById("root");
@@ -30,6 +32,8 @@ function makePageForEpisodes(episodeList) {
   // empty rootElem content to clear the page.
   rootElem.textContent = "";
   episode_count.textContent = `Got ${episodeList.length} episode(s)`;
+
+  populateSelectEpisodeBar(episodeList);
 
   // loop through the episodeList to create each episode card .
   episodeList.forEach((episode) => {
@@ -42,5 +46,42 @@ function makePageForEpisodes(episodeList) {
      </div>`;
   });
 }
+
+// drop down menu for episode and show selection
+
+const select_episode = document.getElementById("episode-select");
+const select_show = document.getElementById("shows-select");
+
+//This function populate the episode bar
+function populateSelectEpisodeBar(episodeList) {
+  select_episode.innerText = "";
+  episodeList.forEach((episode) => {
+    select_episode.innerHTML += `<option value="${episode.id}">S${
+      episode.season < 10 ? "0" + episode.season : episode.season
+    }E${episode.number < 10 ? "0" + episode.number : episode.number} - ${
+      episode.name
+    }</option>`;
+  });
+}
+
+//event handler on change for select episodes, reset after 5 seconds back to full list
+
+function handleChangeEpisodeOption(e) {
+  const allEpisodes2 = getAllEpisodes();
+  console.log(e.target.value);
+  if (e.target.value == "") {
+    makePageForEpisodes(allEpisodes2);
+  } else {
+    let filteredEpisode = allEpisodes2.filter(
+      (episode) => e.target.value == episode.id
+    );
+    makePageForEpisodes(filteredEpisode);
+    setTimeout(() => {
+      makePageForEpisodes(allEpisodes2);
+    }, 5000);
+  }
+}
+
+select_episode.addEventListener("change", handleChangeEpisodeOption);
 
 window.onload = setup;
